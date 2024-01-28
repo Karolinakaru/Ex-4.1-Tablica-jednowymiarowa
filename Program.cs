f65de71c58e90f2aa00323c1381838ccb7d10c0e
singhet <singhet@SCSWL4072>
2024-01-28 16:26:39 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] tablica1 = new int[10]; //size board: 10
+
+        for (int i = 0; i < tablica1.Length; i++)// indeks
+        {
+            tablica1[i] = i;
+        }
+        Console.WriteLine("Index value:  Table value:");
+        for (int i = 0; i < tablica1.Length; i++)
+        {
+            Console.WriteLine($"{i}             {tablica1[i]}");
+        }
+    }
+}
